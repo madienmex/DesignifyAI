4825656af648fb629bee8259d689378857065158
madienmex <madienmex94@gmail.com>
2023-05-16 05:09:05 -0500

    Drop down menu creation and gallery edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,8 +125,7 @@
 		})();
 
 	
-	
-	// Spectral JS code
+	// Spectral JS code **********************
 	
 	// Breakpoints.
 		breakpoints({
@@ -197,3 +196,45 @@
 		}
 		
 })(jQuery);
+
+
+// Helios JS code **********************
+$(document).ready(function(){
+
+	// Dropdowns.
+	$('#nav > ul').dropotron({
+		mode: 'fade',
+		speed: 350,
+		noOpenerFade: true,
+		alignment: 'center'
+	});
+
+	// Nav.
+
+		// Button.
+		$(
+			'<div id="navButton">' +
+				'<a href="#navPanel" class="toggle"></a>' +
+			'</div>'
+		)
+			.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+});
